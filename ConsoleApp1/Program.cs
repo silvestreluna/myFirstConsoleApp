a5f922cd3ccd5d2302f98ff2fcc8ab45192d6928
Silvestre Luna <slvtrluna@gmail.com>
2019-08-20 20:30:26 -0500

    basic of C#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool myBool = true;
+            int myIntValue;
+            myIntValue = 1234;
+
+
+            if (myBool)
+            {
+                Console.WriteLine("We'll never hit this code.");
+            }
+
+            switch (myIntValue)
+            {
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                default:
+                    Console.WriteLine("something");
+                    break;
+            }
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"number is {i}");
+            }
+
+            List<string> myString = new List<string>();
+            myString.Add("heelo");
+            myString.Add("hello2");
+            myString.Insert(1,"hellinsert");
+
+            foreach (var myStrings in myString)
+            {
+                Console.WriteLine(myStrings);
+            }
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
